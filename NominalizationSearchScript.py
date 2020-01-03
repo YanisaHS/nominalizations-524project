8538ac3f4361733a2699193733a951f6163cf5cc
Yanisa Haley Scherber <yhscherber@gmail.com>
2020-01-03 12:12:43 -0600

    Update NominalizationSearchScript.py

--- NominalizationSearchScript.py
+++ NominalizationSearchScript.py
@@ -4,44 +4,39 @@ from nltk.corpus import wordnet as wn
 
 # Part One: Create function to search for nominalizations with NLTK
 
+# Save final nominalizations of words into a dictionary (rather than list) for caching/efficiency
 listOfNominalizedWords = {}
 finalNounsAndCorrespondingVerbs = {}
 knownNotNominalizations = {}
-# Save final nominalizations of words into a dictionary (rather than list) for caching/efficiency
 def nominalizationFunction(word):
-    # print("Looking for nominalizations...")
     if word in finalNounsAndCorrespondingVerbs:
         return True
     if word in knownNotNominalizations:
         return False
+    # Parse for synsets which match "word" (noun) - the synsets picked up will also be nouns
     nounSynsets = wn.synsets(word, pos=wn.NOUN)
-    # Parse for synsets which match "word" (noun) 
-    # The synsets picked up with also be nouns
+    # List the lemmas of the synsets we just identified - these lemmas are still nouns
     for synset in nounSynsets:
         lemmas = synset.lemmas()
-        # List the lemmas of the synsets we just identified
-        # These lemmas are still nouns
+        # Find all lemmas which are "derivationally related" - these will be different syntactic categories
         for lemma in lemmas:
             listOfDRF = lemma.derivationally_related_forms() 
-            # Find all lemmas which are "derivationally related"
-            # Different syntactic categories
             for individualWordInDRF in listOfDRF:
                 wordKey = individualWordInDRF.key()
-                # Creates the WN Key as a string
+                # ^^Creates the WN Key as a string
                 splitStringWordKey = wordKey.split("%")
                 splitStringIndex = splitStringWordKey[0]
+                # Parsing for nominalizations - must be verbs and match first 3 characters
                 if splitStringWordKey[1][0] != "1" and splitStringIndex[0:3] == word[0:3]:
                     if len(splitStringIndex) <= len(word):
-                    # Parsing for nominalizations - must be verbs and match first 3 characters
                         finalRoots = wordKey.partition("%")[0]
-                        # Create the list of verbs which are can create nominalizations of the word
+                        # Add nominalizations to lists to track frequency & words
                         nomTracker(word)
                         finalNounsAndCorrespondingVerbs[word] = finalRoots
                         return True
                         # If nominaliztion is found, function ends here because it does not have to
                         #   check the other words (it doesn't matter)
     knownNotNominalizations[word] = ""
-    # print("These are extra:", knownNotNominalizations)
     return False
 
 # Below function is to keep track of what words are nominalizations and their frequencies
@@ -58,8 +53,8 @@ cachedNOMLEXPLUS = set()
 openNOMLEXPLUS = open("/Users/yanisa/Google Drive/Data/cleanNOMLEXPLUS.txt")
 print("Loading texts...")
 for individualWordInNOMLEX in openNOMLEXPLUS:
+    # Removie the formatting for a new line
     individualWordInNOMLEX = individualWordInNOMLEX.rstrip("\n")
-    # Removing the formatting for a new line
     cachedNOMLEXPLUS.add(individualWordInNOMLEX)
 
 # Part Three: Run nlp function in spaCy on text to POS-tag it
@@ -68,8 +63,6 @@ openTextFile = open("/Users/yanisa/Google Drive/School/Homework - Grad School/Uo
 nlp = spacy.load("en_core_web_sm")
 nlp.max_length = 8000000
 textSamples = nlp(openTextFile.read())
-# for token in textSamples:
-    # print(token.pos_, token.text)
 print("Files uploaded and POS-tagged.")
 
 # Part Four: Search for nouns and compare nouns to list of nominalizations in NOMLEX PLUS
@@ -89,13 +82,10 @@ for wordInSample in textSamples:
     totalWords = totalWords + 1
     if wordInSample.pos_ == "NOUN":
         totalNouns = totalNouns + 1
-        #if (wordInSample.text.endswith("s") or wordInSample.text.endswith("es")) and (not wordInSample.text.endswith("ings")):
-            #continue
-            # continue brings it back up to re-start the loop
         if wordInSample.text in cachedNOMLEXPLUS:
             totalNumberOfNominalizations = totalNumberOfNominalizations + 1
+            # Count the number of nominalizations which match in NOMLEX PLUS
             nomTracker(wordInSample.text)
-            # Counted the number of nominalizations which match in NOMLEX PLUS
 
 # Part Five: For nouns not captured in NOMLEX PLUS, parse through NLTK to see if they are nominalizations
 
@@ -103,13 +93,7 @@ for wordInSample in textSamples:
             if nominalizationFunction(wordInSample.text) == True:
                 totalNumberOfNominalizations = totalNumberOfNominalizations + 1
 
-# print(finalNounsAndCorrespondingVerbs)
 print("The total number of words is: ", totalWords)
 print("The total number of nouns is: ", totalNouns)
 print("The total number of nominalizations is: ", totalNumberOfNominalizations)
 print("The total frequency of nominalizations in the text is: ", totalNumberOfNominalizations / totalWords)
- 
-# listOfNominalizedWordsForCSV = open("/Users/yanisa/Google Drive/School/Homework - Grad School/UofA/EN 524 English Structure/FrequencyData/VisualPerformingArtsCSVData.csv", "w")
-# for word, frequency in listOfNominalizedWords.items():
-#     listOfNominalizedWordsForCSV.write(word + "," + str(frequency) + "\n")
-
